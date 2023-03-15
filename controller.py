44e8e960299b70bd0b50748dbf724b8a08861562
Anton <tosha.hd2@gmail.com>
2023-03-15 08:32:55 +0300

    searchidModule done

--- controller.py
+++ controller.py
@@ -1,27 +1,40 @@
 from addModule import *
 from readModule import *
 from printModule import *
+from searchIdModule import *
+
 
 def start():
     print("Выберите команду приложения:\n\
     1. Показать все заметки.\n\
-    2. Добавить новую заметку.\n\
-    3. Изменить заметку.\n\
-    4. Удалить заметку.")
+    2. Показать заметки в диапазоне дат.\n\
+    3. Показать заметку по номеру ID.\n\
+    4. Добавить новую заметку.\n\
+    5. Изменить заметку по номеру ID.\n\
+    6. Удалить заметку по номеру ID.")
     mode = input("Введите номер команды: ")
     while True:
         if mode == '1':
             data = read_data()
             print_data(data)
             start()
-        elif mode == '2':
+        elif mode == '3':
+            idRequest = input("Введите ID для поиска заметки: ")
+            data = read_data()
+            item = search_data(idRequest, data)
+            if item != None:
+                print_data(item, True)
+            else:
+                print("ID не найден.")
+            start()
+        elif mode == "4":
             push_data()
             print("Заметка сохранена.")
             start()
-        elif mode == '3':
+        elif mode == '5':
             print("Заметка сохранена.")
             start()
-        elif mode == '4':
+        elif mode == '6':
             print("Заметка удалена.")
             start()
         else:
--- printModule.py
+++ printModule.py
@@ -1,6 +1,7 @@
-def print_data(data):
+def print_data(data, search = False):
     if len(data) > 0:
-        del data[0]
+        if not search:
+            del data[0]
         for content in data:
             print('id: ', content[0], "\n", 'Заголовок: ', content[1], "\n", 'Заметка: ', content[2], "\n", 'Дата изменения: ', content[3], "\n")
     else:
